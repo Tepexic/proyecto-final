f117d650bfbfd41c7c44a0843c4b2699917aede1
Jesus6Connex <jesus.velazquez@6connex.com>
2021-10-31 14:15:06 -0600

    create admin management route

---
+++ routes/admin.js
@@ -0,0 +1,25 @@
+/**
+ * El router base '/api/admin' implementará dos funcionalidades:
+ * - GET: obtiene si el usuario es administrador o no
+ * - POST: cambia el estado de administrador del usuario
+ */
+
+const AdminManager = require("./../utils/AdminManager");
+const adminManager = new AdminManager("./routes/data/admin.json");
+
+const express = require('express');
+const { Router } = express;
+
+const adminRouter = Router();
+
+adminRouter.get('/', async (req, res) => {
+  const isAdmin =  await adminManager.isAdmin();
+  res.json(isAdmin);
+});
+
+adminRouter.post('/', async (req, res) => {
+  const isAdmin = await adminManager.toggleAdmin();
+  res.status(200).json(isAdmin);
+});
+
+module.exports = adminRouter;
