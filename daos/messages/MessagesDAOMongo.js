2dea7ec9b43f1f3d5c34c4e2b2971dc931198015
Jesus Velazquez <jebus.velazquez@gmail.com>
2022-04-14 19:43:20 -0500

    fix: Fix messages dao

--- daos/messages/MessagesDAOMongo.js
+++ daos/messages/MessagesDAOMongo.js
@@ -1,21 +1,27 @@
 const Contenedor = require("./../../containers/Contenedor_mongo");
 const { Schema } = require("mongoose");
 
-const messagesSchema = new Schema(
-  {
-    author: String,
-    text: String,
-    timestamp: String,
-  },
-  {
-    writeConcern: {
-      w: "majority",
-      j: true,
-      wtimeout: 1000,
-    },
+class Messages extends Contenedor {
+  constructor() {
+    super(
+      "messages",
+      new Schema(
+        {
+          author: String,
+          text: String,
+          timestamp: String,
+          avatar: String,
+        },
+        {
+          writeConcern: {
+            w: "majority",
+            j: true,
+            wtimeout: 1000,
+          },
+        }
+      )
+    );
   }
-);
-
-const Messages = new Contenedor("messages", messagesSchema);
+}
 
 module.exports = Messages;
